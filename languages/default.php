dbd9239a0fe8c1dd541a3102427cf526b798983e
cmb69 <cmbecker69@gmx.de>
2012-03-26 13:51:37 +0000

    added default.php

---
+++ languages/default.php
@@ -0,0 +1,23 @@
+<?php
+
+$plugin_tx['imagescroller']['syscheck_title']="System check";
+$plugin_tx['imagescroller']['syscheck_phpversion']="PHP version ≥ %s";
+$plugin_tx['imagescroller']['syscheck_extension']="Extension '%s' loaded";
+$plugin_tx['imagescroller']['syscheck_encoding']="Encoding 'UTF-8' configured";
+$plugin_tx['imagescroller']['syscheck_magic_quotes']="Magic quotes runtime off";
+$plugin_tx['imagescroller']['syscheck_jquery']="<a href=\"http://www.cmsimple-xh.com/wiki/doku.php/plugins:jquery4cmsimple\">jQuery4CMSimple plugin</a> installed";
+$plugin_tx['imagescroller']['syscheck_writable']="Folder '%s' writable";
+
+$plugin_tx['imagescroller']['button_prev']="Previous Image";
+$plugin_tx['imagescroller']['button_next']="Next Image";
+$plugin_tx['imagescroller']['button_play']="Continue slideshow";
+$plugin_tx['imagescroller']['button_stop']="Pause slideshow";
+
+$plugin_tx['imagescroller']['error_no_image']="No image:";
+$plugin_tx['imagescroller']['error_image_size']="Image size %1dx%2d (expected %3dx%4d):";
+
+$plugin_tx['imagescroller']['cf_scroll_interval']="The interval between two scroll processes in milliseconds.";
+$plugin_tx['imagescroller']['cf_scroll_duration']="The duration of scrolling to the next image in milliseconds.";
+$plugin_tx['imagescroller']['cf_autoload']="Wether to autoload the necessary JS: 1 (yes) or 0 (no). Set to 1, if used from the template.";
+
+?>
