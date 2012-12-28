c616279876174ac726a20268a055aa6d563d1f74
cmb69 <cmbecker69@gmx.de>
2012-12-28 13:51:49 +0000

    added Slovak localization

---
+++ languages/sk.php
@@ -0,0 +1,23 @@
+<?php
+
+$plugin_tx['imagescroller']['syscheck_title']="Kontzrola systému";
+$plugin_tx['imagescroller']['syscheck_phpversion']="PHP verzia ≥ %s";
+$plugin_tx['imagescroller']['syscheck_extension']="Rozšírenie '%s' načítané";
+$plugin_tx['imagescroller']['syscheck_encoding']="Kódovanie 'UTF-8' nastavené";
+$plugin_tx['imagescroller']['syscheck_magic_quotes']="Magic quotes runtime off";
+$plugin_tx['imagescroller']['syscheck_jquery']="<a href=\"http://www.cmsimple-xh.com/wiki/doku.php/plugins:jquery4cmsimple\">jQuery4CMSimple plugin</a> nainštalovaný";
+$plugin_tx['imagescroller']['syscheck_writable']="Adresár '%s' umožňuje zápis";
+
+$plugin_tx['imagescroller']['button_prev']="Predchádzajúci obrázok";
+$plugin_tx['imagescroller']['button_next']="Nasledujúci obrázok";
+$plugin_tx['imagescroller']['button_play']="Pokračovať v slideshow";
+$plugin_tx['imagescroller']['button_stop']="Zastaviť slideshow";
+
+$plugin_tx['imagescroller']['error_no_image']="Žiadne obrázky:";
+$plugin_tx['imagescroller']['error_image_size']="Veľkosť obrázku %1dx%2d (očakávaná %3dx%4d):";
+
+$plugin_tx['imagescroller']['cf_scroll_interval']="Interval medzi dvomi premenami v milisekundách.";
+$plugin_tx['imagescroller']['cf_scroll_duration']="Trvanie premeny dvoch obrázkov v milisekundách.";
+$plugin_tx['imagescroller']['cf_autoload']="Majú sa vopred načítať potrebné JS? 1 (áno) alebo 0 (nie). Ak používate plugin v šablóne, nastavte 1.";
+
+?>
