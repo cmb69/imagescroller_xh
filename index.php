f7a6e8f5862d30675e7efb0a30c5ceb40806bb9d
cmb69 <cmbecker69@gmx.de>
2012-12-28 17:06:05 +0000

    made minor code cosmetics

--- index.php
+++ index.php
@@ -13,23 +13,27 @@ if (!defined('CMSIMPLE_XH_VERSION')) {
 }
 
 
+define('IMAGESCROLLER_VERSION', '1beta3');
+
+
 /**
  * Returns the sorted array of images in $dir.
  *
  * @param string $dir
  * @return array
  */
-function imagescroller_images_from_dir($dir) {
-    $dir = rtrim($dir, '/').'/';
+function Imagescroller_imagesFromDir($dir)
+{
+    $dir = rtrim($dir, '/') . '/';
     $imgs = array();
     if (($dh = opendir($dir)) !== FALSE) {
-	while (($fn = readdir($dh)) !== FALSE) {
-	    $ffn = $dir.$fn;
-	    if (is_file($ffn) && getimagesize($ffn)) {
-		$imgs[] = $ffn;
-	    }
-	}
-	closedir($dh);
+        while (($fn = readdir($dh)) !== FALSE) {
+            $ffn = $dir . $fn;
+            if (is_file($ffn) && getimagesize($ffn)) {
+                $imgs[] = $ffn;
+            }
+        }
+        closedir($dh);
     }
     natcasesort($imgs);
     return $imgs;
@@ -42,15 +46,16 @@ function imagescroller_images_from_dir($dir) {
  * @param string $fn
  * @return array
  */
-function imagescroller_images_from_file($fn) {
-    $dir = dirname($fn).'/';
+function Imagescroller_imagesFromFile($fn)
+{
+    $dir = dirname($fn) . '/';
     $data = file_get_contents($fn);
     $data = str_replace(array("\r\n", "\r"), "\n", $data);
     $recs = explode("\n\n", $data);
     foreach ($recs as $rec) {
-	$rec = array_map('trim', explode("\n", $rec));
-	$rec[0] = $dir.$rec[0];
-	$res[] = $rec;
+        $rec = array_map('trim', explode("\n", $rec));
+        $rec[0] = $dir . $rec[0];
+        $res[] = $rec;
     }
     return $res;
 }
@@ -64,27 +69,28 @@ function imagescroller_images_from_file($fn) {
  * @param array $images
  * @returns array
  */
-function imagescroller_images_size($imgs) {
+function Imagescroller_imagesSize($imgs)
+{
     global $e, $adm, $plugin_tx;
 
     $ptx = $plugin_tx['imagescroller'];
     foreach ($imgs as $img) {
-	$fn = is_array($img) ? $img[0] : $img;
-	if (!is_readable($fn) || !($size = getimagesize($fn))) {
-	    $e = '<li><strong>'.$ptx['error_no_image'].'</strong>'
-		    .tag('br').$fn.'</li>'."\n";
-	    continue;
-	}
-	if (!isset($width)) {
-	    list($width, $height) = $size;
-	} else {
-	    if (($size[0] != $width || $size[1] != $height) && $adm) {
-		$e .= '<li><strong>'
-			.sprintf($plugin_tx['imagescroller']['error_image_size'],
-				$size[0], $size[1], $width, $height)
-			.'</strong>'.tag('br').$fn.'</li>'."\n";
-	    }
-	}
+        $fn = is_array($img) ? $img[0] : $img;
+        if (!is_readable($fn) || !($size = getimagesize($fn))) {
+            $e = "<li><strong>$ptx[error_no_image]</strong>"
+                . tag('br') . "$fn</li>";
+            continue;
+        }
+        if (!isset($width)) {
+            list($width, $height) = $size;
+        } else {
+            if (($size[0] != $width || $size[1] != $height) && $adm) {
+                $e .= '<li><strong>'
+                    . sprintf($ptx['error_image_size'],
+                              $size[0], $size[1], $width, $height)
+                    . '</strong>' . tag('br') . "$fn</li>";
+            }
+        }
     }
     return array($width, $height);
 }
@@ -98,27 +104,28 @@ function imagescroller_images_size($imgs) {
  * @param int $height
  * @return string  The (X)HTML.
  */
-function imagescroller_image_li($img, $width, $height) {
+function Imagescroller_imageLi($img, $width, $height)
+{
     if (is_array($img)) {
-	list($fn, $url, $title, $desc) = $img;
+        list($fn, $url, $title, $desc) = $img;
     } else {
-	$fn = $img; $url = $title = $desc = NULL;
+        $fn = $img; $url = $title = $desc = NULL;
     }
     $o = '<li>'
-	    .(!empty($url) ? '<a href="'.$url.'">' : '')
-	    .tag('img src="'.$fn.'" alt="" width="'.$width.'" height="'.$height.'"')
-	    .(!empty($url) ? '</a>' : '');
+        . (!empty($url) ? "<a href=\"$url\">" : '')
+        . tag("img src=\"$fn\" alt=\"\" width=\"$width\" height=\"$height\"")
+        . (!empty($url) ? '</a>' : '');
     if (!empty($title) || !empty($desc)) {
-	$o .= '<div class="imagescroller_info">'
-		.'<h6>'
-		.(!empty($url) ? '<a href="'.$url.'">' : '')
-		.$title
-		.(!empty($url) ? '</a>' : '')
-		.'</h6>'
-		.'<p>'.$desc.'</p>'
-		.'</div>';
+        $o .= '<div class="imagescroller_info">'
+            . '<h6>'
+            . (!empty($url) ? "<a href=\"$url\">" : '')
+            . $title // htmlspecialchars?
+            . (!empty($url) ? '</a>' : '')
+            . '</h6>'
+            . "<p>$desc</p>" // Htmlspecialchars?
+            . '</div>';
     }
-    $o .= '</li>'."\n";
+    $o .= '</li>';
     return $o;
 }
 
@@ -130,61 +137,65 @@ function imagescroller_image_li($img, $width, $height) {
  * @global $hjs
  * @return void
  */
-function imagescroller_js() {
+function Imagescroller_js()
+{
     global $pth, $hjs, $plugin_cf;
-    static $again = FALSE;
+    static $again = false;
 
-    if ($again) {return;}
-    $again = TRUE;
+    if ($again) {
+        return;
+    }
+    $again = true;
     $pcf = $plugin_cf['imagescroller'];
-    include_once $pth['folder']['plugins'].'jquery/jquery.inc.php';
+    include_once $pth['folder']['plugins'] . 'jquery/jquery.inc.php';
     include_jquery();
     include_jqueryplugin('scrollTo', $pth['folder']['plugins']
-	    .'imagescroller/lib/jquery.scrollTo-1.4.3.1-min.js');
+        . 'imagescroller/lib/jquery.scrollTo-1.4.3.1-min.js');
     include_jqueryplugin('serialScroll', $pth['folder']['plugins']
-	    .'imagescroller/lib/jquery.serialScroll-1.2.2-min.js');
+        . 'imagescroller/lib/jquery.serialScroll-1.2.2-min.js');
     $dynctrls = $pcf['controls_dynamic'] ? 'true' : 'false';
     $hjs .= <<<SCRIPT
 <script type="text/javascript">
 /* <![CDATA[ */
 (function($) {
     $(function() {
-	$('div.imagescroller').serialScroll({
-	    items: 'li',
-	    prev: 'div.imagescroller_container img.imagescroller_prev',
-	    next: 'div.imagescroller_container img.imagescroller_next',
-	    force: true,
-	    axis: 'xy',
-	    duration: $pcf[scroll_duration],
-	    interval: $pcf[scroll_interval],
-	    constant: false
-	});
-	if ($dynctrls) {
-	    $('div.imagescroller_container').mouseenter(function() {
-		$(this).find('img.imagescroller_prev, img.imagescroller_next, img.imagescroller_play, img.imagescroller_stop').show()
-	    }).mouseleave(function() {
-		$(this).find('img.imagescroller_prev, img.imagescroller_next, img.imagescroller_play, img.imagescroller_stop').hide()
-	    });
-	    $('img.imagescroller_stop').click(function() {
-		$('div.imagescroller').trigger('stop');
-		$('img.imagescroller_stop').css('visibility', 'hidden');
-		$('img.imagescroller_play').css('visibility', 'visible');
-	    });
-	    $('img.imagescroller_play').click(function() {
-		$('div.imagescroller').trigger('start');
-		$('img.imagescroller_play').css('visibility', 'hidden');
-		$('img.imagescroller_stop').css('visibility', 'visible');
-	    })
-	} else {
-	    $(this).find('img.imagescroller_prev, img.imagescroller_next').show()
-	}
+        $('div.imagescroller').serialScroll({
+            items: 'li',
+            prev: 'div.imagescroller_container img.imagescroller_prev',
+            next: 'div.imagescroller_container img.imagescroller_next',
+            force: true,
+            axis: 'xy',
+            duration: $pcf[scroll_duration],
+            interval: $pcf[scroll_interval],
+            constant: false
+        });
+        if ($dynctrls) {
+            $('div.imagescroller_container').mouseenter(function() {
+                $(this).find('img.imagescroller_prev, img.imagescroller_next,
+			      img.imagescroller_play, img.imagescroller_stop').show();
+            }).mouseleave(function() {
+                $(this).find('img.imagescroller_prev, img.imagescroller_next,
+			      img.imagescroller_play, img.imagescroller_stop').hide();
+            });
+            $('img.imagescroller_stop').click(function() {
+                $('div.imagescroller').trigger('stop');
+                $('img.imagescroller_stop').css('visibility', 'hidden');
+                $('img.imagescroller_play').css('visibility', 'visible');
+            });
+            $('img.imagescroller_play').click(function() {
+                $('div.imagescroller').trigger('start');
+                $('img.imagescroller_play').css('visibility', 'hidden');
+                $('img.imagescroller_stop').css('visibility', 'visible');
+            })
+        } else {
+            $(this).find('img.imagescroller_prev, img.imagescroller_next').show()
+        }
     })
 })(jQuery)
 /* ]]> */
 </script>
 
 SCRIPT;
-
 }
 
 
@@ -195,33 +206,34 @@ SCRIPT;
  * @param string $path  A directory of info file.
  * @return string  The (X)HTML.
  */
-function imagescroller($path) {
+function imagescroller($path)
+{
     global $pth, $plugin_tx;
 
     $imgs = is_dir($path)
-	    ? imagescroller_images_from_dir($path)
-	    : imagescroller_images_from_file($path);
-    list($width, $height) = imagescroller_images_size($imgs);
-    imagescroller_js();
-    $o = '<div class="imagescroller_container" style="width: '.$width.'px; height: '.$height.'px">'."\n"
-	    .'<div class="imagescroller" style="width: '.$width.'px; height: '.$height.'px">'."\n"
-	    .'<ul style="width: '.count($imgs) * $width.'px; height: '.$height.'px">'."\n";
+	? imagescroller_imagesFromDir($path)
+	: imagescroller_imagesFromFile($path);
+    list($width, $height) = Imagescroller_imagesSize($imgs);
+    Imagescroller_js();
+    $o = "<div class=\"imagescroller_container\" style=\"width:${width}px; height:${height}px\">"
+        . "<div class=\"imagescroller\" style=\"width:${width}px; height:${height}px\">"
+        . '<ul style="width:' . count($imgs) * $width . "px; height:${height}px\">";
     foreach ($imgs as $img) {
-	$o .= imagescroller_image_li($img, $width, $height);
+        $o .= Imagescroller_imageLi($img, $width, $height);
     }
-    $o .= '</ul>'."\n";
-    $o .= '</div>'."\n";
+    $o .= '</ul>';
+    $o .= '</div>';
     foreach (array('prev', 'next', 'play', 'stop') as $btn) {
-	$name = $btn;
-	$alt = $plugin_tx['imagescroller']['button_'.$btn];
-	$img = $pth['folder']['plugins'].'imagescroller/images/'.$name.'.png';
-	list($w, $h) = getimagesize($img);
-	$top = 'top:'.intval(($height - $h) / 2).'px;';
-	$left = $btn == 'play' || $btn == 'stop' ? 'left:'.intval(($width - $w) / 2).'px' : '';
-	$o .= tag('img class="imagescroller_'.$btn.'" src="'.$img.'" alt="'.$alt.'"'
-		.' style="'.$top.$left.'"')."\n";
+        $name = $btn;
+        $alt = $plugin_tx['imagescroller']["button_$btn"];
+        $img = "{$pth['folder']['plugins']}imagescroller/images/$name.png";
+        list($w, $h) = getimagesize($img);
+        $top = 'top:' . intval(($height - $h) / 2) . 'px;';
+        $left = $btn == 'play' || $btn == 'stop' ? 'left:' . intval(($width - $w) / 2) . 'px' : '';
+        $o .= tag("img class=\"imagescroller_$btn\" src=\"$img\" alt=\"$alt\""
+		  . " style=\"$top$left\"");
     }
-    $o .= '</div>'."\n";
+    $o .= '</div>';
     return $o;
 }
 
@@ -230,7 +242,7 @@ function imagescroller($path) {
  * Handle autoloading of necessary JS.
  */
 if ($plugin_cf['imagescroller']['autoload']) {
-    imagescroller_js();
+    Imagescroller_js();
 }
 
 ?>
