a6b287e5b4eb541cde4d14e46f04d8bc6a477e4f
cmb69 <cmbecker69@gmx.de>
2013-03-20 21:50:15 +0000

    added Estonian localization (contributed by tanavots)

---
+++ languages/et.php
@@ -0,0 +1,20 @@
+<?php
+
+	$plugin_tx['imagescroller']['syscheck_title']="Keskkonna kontroll";
+	$plugin_tx['imagescroller']['syscheck_phpversion']="PHP versioon ≥ %s";
+	$plugin_tx['imagescroller']['syscheck_extension']="Laiendus '%s' on laaditud";
+	$plugin_tx['imagescroller']['syscheck_encoding']="'UTF-8' kodeering on seadistatud";
+	$plugin_tx['imagescroller']['syscheck_magic_quotes']="Magic quotes runtime välja lülitatud";
+	$plugin_tx['imagescroller']['syscheck_jquery']="<a href=\"http://www.cmsimple-xh.com/wiki/doku.php/plugins:jquery4cmsimple\">jQuery4CMSimple lisa</a> on paigaldatud";
+	$plugin_tx['imagescroller']['syscheck_writable']="Kaust '%s' on kirjutatav";
+	$plugin_tx['imagescroller']['button_prev']="Eelmine foto";
+	$plugin_tx['imagescroller']['button_next']="Järgmine foto";
+	$plugin_tx['imagescroller']['button_play']="Jätka slaidiseanssi";
+	$plugin_tx['imagescroller']['button_stop']="Peata slaidiseanss";
+	$plugin_tx['imagescroller']['error_no_image']="Fotod puuduvad:";
+	$plugin_tx['imagescroller']['error_image_size']="Foto suurus %1dx%2d (eeldatav %3dx%4d):";
+	$plugin_tx['imagescroller']['cf_scroll_interval']="Kahe kerimise vaheline intervall millisekundites.";
+	$plugin_tx['imagescroller']['cf_scroll_duration']="Järgmise pildi kerimise aeg millisekundites.";
+	$plugin_tx['imagescroller']['cf_autoload']="Kui JS automaatne laadimine on vajalik: 1 (jah) või 0 (ei). Valige 1, kui kasutatate malliga.";
+
+?>
