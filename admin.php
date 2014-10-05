3f745b623e57b412fc900da1c721ccd6900de5fc
cmb69 <cmbecker69@gmx.de>
2014-10-05 19:36:56 +0000

    bumped copyright year

--- admin.php
+++ admin.php
@@ -8,7 +8,7 @@
  * @category  CMSimple_XH
  * @package   Imagescroller
  * @author    Christoph M. Becker <cmbecker69@gmx.de>
- * @copyright 2012-2013 Christoph M. Becker <http://3-magi.net>
+ * @copyright 2012-2014 Christoph M. Becker <http://3-magi.net>
  * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
  * @version   SVN: $Id$
  * @link      http://3-magi.net/?CMSimple_XH/Imagescroller_XH
@@ -43,7 +43,7 @@ function Imagescroller_version()
 <img src="$iconPath" with="128" height="128"
      style="float: left; margin: 0 1em 0 0" alt="Plugin Icon" />
 <p>Version: $version</p>
-<p>Copyright &copy; 2012-2013 <a href="http://3-magi.net">
+<p>Copyright &copy; 2012-2014 <a href="http://3-magi.net">
     Christoph M. Becker</a></p>
 <p>Imagescroller_XH is powered by <a
     href="http://flesler.blogspot.de/2008/02/jqueryserialscroll.html">
