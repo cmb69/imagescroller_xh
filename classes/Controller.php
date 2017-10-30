cmb69/imagescroller_xh

commit 622b4161297f82a614818631b8d8137998d77f86

refactored

classes/Controller.php
@@ -98,126 +98,6 @@ class Imagescroller_Controller
     }
 
     /**
-     * Returns the sorted array of images in a folder.
-     *
-     * @param string $dir A folder path.
-     *
-     * @return array
-     */
-    protected function imagesFromDir($dir)
-    {
-        $dir = rtrim($dir, '/') . '/';
-        $imgs = array();
-        if (($dh = opendir($dir)) !== false) {
-            while (($fn = readdir($dh)) !== false) {
-                $ffn = $dir . $fn;
-                if (is_file($ffn) && getimagesize($ffn)) {
-                    $imgs[] = $ffn;
-                }
-            }
-            closedir($dh);
-        }
-        natcasesort($imgs);
-        return $imgs;
-    }
-
-    /**
-     * Returns the array of images in an info file.
-     *
-     * @param string $fn An info file name.
-     *
-     * @return array
-     */
-    protected function imagesFromFile($fn)
-    {
-        $dir = dirname($fn) . '/';
-        $data = file_get_contents($fn);
-        $data = str_replace(array("\r\n", "\r"), "\n", $data);
-        $recs = explode("\n\n", $data);
-        foreach ($recs as $rec) {
-            $rec = array_map('trim', explode("\n", $rec));
-            $rec[0] = $dir . $rec[0];
-            $res[] = $rec;
-        }
-        return $res;
-    }
-
-    /**
-     * Returns the dimensions of the $imgs.
-     *
-     * If the dimensions differ, this will be reported through $e in admin mode.
-     *
-     * @param array $imgs A list of images.
-     *
-     * @return array
-     *
-     * @global string The (X)HTML containing error messages.
-     * @global bool   Whether we're in admin mode.
-     * @global array  The localization of the plugins.
-     */
-    protected function imagesSize($imgs)
-    {
-        global $e, $adm, $plugin_tx;
-
-        $ptx = $plugin_tx['imagescroller'];
-        foreach ($imgs as $img) {
-            $fn = is_array($img) ? $img[0] : $img;
-            if (!is_readable($fn) || !($size = getimagesize($fn))) {
-                $e = "<li><strong>$ptx[error_no_image]</strong>"
-                    . tag('br') . "$fn</li>";
-                continue;
-            }
-            if (!isset($width)) {
-                list($width, $height) = $size;
-            } else {
-                if (($size[0] != $width || $size[1] != $height) && $adm) {
-                    $e .= '<li><strong>'
-                        . sprintf(
-                            $ptx['error_image_size'],
-                            $size[0], $size[1], $width, $height
-                        )
-                        . '</strong>' . tag('br') . "$fn</li>";
-                }
-            }
-        }
-        return array($width, $height);
-    }
-
-    /**
-     * Returns the <li> containing the image.
-     *
-     * @param mixed $img    An image.
-     * @param int   $width  An image width.
-     * @param int   $height An image height.
-     *
-     * @return string (X)HTML.
-     */
-    protected function imageLi($img, $width, $height)
-    {
-        if (is_array($img)) {
-            list($fn, $url, $title, $desc) = $img;
-        } else {
-            $fn = $img; $url = $title = $desc = null;
-        }
-        $o = '<li>'
-            . (!empty($url) ? "<a href=\"$url\">" : '')
-            . tag("img src=\"$fn\" alt=\"\" width=\"$width\" height=\"$height\"")
-            . (!empty($url) ? '</a>' : '');
-        if (!empty($title) || !empty($desc)) {
-            $o .= '<div class="imagescroller_info">'
-                . '<h6>'
-                . (!empty($url) ? "<a href=\"$url\">" : '')
-                . $title // htmlspecialchars?
-                . (!empty($url) ? '</a>' : '')
-                . '</h6>'
-                . "<p>$desc</p>" // Htmlspecialchars?
-                . '</div>';
-        }
-        $o .= '</li>';
-        return $o;
-    }
-
-    /**
      * Includes the necessary JS.
      *
      * @return void
@@ -240,58 +120,24 @@ class Imagescroller_Controller
         $pcf = $plugin_cf['imagescroller'];
         include_once $pth['folder']['plugins'] . 'jquery/jquery.inc.php';
         include_jquery();
+        $libraryFolder =  $pth['folder']['plugins'] . 'imagescroller/lib/';
         include_jqueryplugin(
-            'scrollTo', $pth['folder']['plugins']
-            . 'imagescroller/lib/jquery.scrollTo-1.4.3.1-min.js'
+            'scrollTo', $libraryFolder . 'jquery.scrollTo-1.4.3.1-min.js'
         );
         include_jqueryplugin(
-            'serialScroll', $pth['folder']['plugins']
-            . 'imagescroller/lib/jquery.serialScroll-1.2.2-min.js'
+            'serialScroll', $libraryFolder . 'jquery.serialScroll-1.2.2-min.js'
         );
-        $fastRewind = $pcf['rewind_fast'] ? 'false' : 'true';
-        $dynctrls = $pcf['controls_dynamic'] ? 'true' : 'false';
-        $hjs .= <<<EOT
-<script type="text/javascript">
-/* <![CDATA[ */
-(function($) {
-    $(function() {
-        $('div.imagescroller').serialScroll({
-            items: 'li',
-            prev: 'div.imagescroller_container img.imagescroller_prev',
-            next: 'div.imagescroller_container img.imagescroller_next',
-            force: true,
-            axis: 'xy',
-            duration: $pcf[scroll_duration],
-            interval: $pcf[scroll_interval],
-            constant: $fastRewind
-        });
-        if ($dynctrls) {
-            $('div.imagescroller_container').mouseenter(function() {
-                $(this).find('img.imagescroller_prev, img.imagescroller_next,' +
-                        'img.imagescroller_play, img.imagescroller_stop').show();
-            }).mouseleave(function() {
-                $(this).find('img.imagescroller_prev, img.imagescroller_next,' +
-                        'img.imagescroller_play, img.imagescroller_stop').hide();
-            });
-            $('img.imagescroller_stop').click(function() {
-                $('div.imagescroller').trigger('stop');
-                $('img.imagescroller_stop').css('visibility', 'hidden');
-                $('img.imagescroller_play').css('visibility', 'visible');
-            });
-            $('img.imagescroller_play').click(function() {
-                $('div.imagescroller').trigger('start');
-                $('img.imagescroller_play').css('visibility', 'hidden');
-                $('img.imagescroller_stop').css('visibility', 'visible');
-            })
-        } else {
-            $(this).find('img.imagescroller_prev, img.imagescroller_next').show()
-        }
-    })
-})(jQuery)
-/* ]]> */
-</script>
-
-EOT;
+        $config = array(
+            'duration' => (int) $pcf['scroll_duration'],
+            'interval' => (int) $pcf['scroll_interval'],
+            'constant' => (bool) $pcf['rewind_fast'],
+            'dynamicControls' => (bool) $pcf['controls_dynamic']
+        );
+        $hjs .= '<script type="text/javascript">/* <![CDATA[ */'
+            . 'var IMAGESCROLLER = ' . XH_encodeJson($config) . ';'
+            . '/* ]]> */</script>'
+            . '<script type="text/javascript" src="' . $pth['folder']['plugins']
+            . 'imagescroller/imagescroller.js"></script>';
     }
 
     /**
@@ -308,39 +154,75 @@ EOT;
     {
         global $pth, $plugin_tx;
 
-        $imgs = is_dir($path)
-            ? $this->imagesFromDir($path)
-            : $this->imagesFromFile($path);
-        list($width, $height) = $this->imagesSize($imgs);
+        $gallery = is_dir($path)
+            ? Imagescroller_Gallery::makeFromFolder($path)
+            : Imagescroller_Gallery::makeFromFile($path);
+        list($width, $height) = $gallery->getDimensions();
         $this->emitJs();
-        $totalWidth = count($imgs) * $width;
-        $o = <<<EOT
-<div class="imagescroller_container" style="width:{$width}px; height:{$height}px">
-    <div class="imagescroller" style="width:{$width}px; height:{$height}px">
-        <ul style="width:{$totalWidth}px; height:{$height}px\">
+        $totalWidth = $gallery->getImageCount() * $width;
+        return $this->render(
+            'gallery', compact('gallery', 'width', 'height', 'totalWidth')
+        );
+    }
 
-EOT;
-        foreach ($imgs as $img) {
-            $o .= $this->imageLi($img, $width, $height);
+    /**
+     * Renders a template.
+     *
+     * @param string $template A template name.
+     * @param array  $bag      A bag with template variables.
+     *
+     * @return string (X)HTML.
+     *
+     * @global array The paths of system files and folders.
+     * @global array The configuration of the core.
+     */
+    protected function render($template, $bag)
+    {
+        global $pth, $cf;
+
+        ob_start();
+        extract($bag);
+        include $pth['folder']['plugins'] . 'imagescroller/views/' . $template
+            . '.htm';
+        $html = ob_get_clean();
+        if (!$cf['xhtml']['endtags']) {
+            $html = str_replace(' />', '>', $html);
         }
-        $o .= '</ul>';
-        $o .= '</div>';
+        return $html;
+    }
+
+    /**
+     * Renders the buttons.
+     *
+     * @param int $width  A width.
+     * @param int $height A height.
+     *
+     * @return string (X)HTML.
+     *
+     * @global array The paths of system files and folders.
+     * @global array The localization of the plugins.
+     */
+    protected function renderButtons($width, $height)
+    {
+        global $pth, $plugin_tx;
+
+        $html = '';
         foreach (array('prev', 'next', 'play', 'stop') as $btn) {
             $name = $btn;
-            $alt = $plugin_tx['imagescroller']["button_$btn"];
-            $img = "{$pth['folder']['plugins']}imagescroller/images/$name.png";
+            $alt = $plugin_tx['imagescroller']['button_' . $btn];
+            $img = $pth['folder']['plugins'] . 'imagescroller/images/' . $name
+                . '.png';
             list($w, $h) = getimagesize($img);
             $top = 'top:' . intval(($height - $h) / 2) . 'px;';
             $left = ($btn == 'play' || $btn == 'stop')
                 ? 'left:' . intval(($width - $w) / 2) . 'px'
                 : '';
-            $o .= tag(
-                "img class=\"imagescroller_$btn\" src=\"$img\" alt=\"$alt\""
-                . " style=\"$top$left\""
+            $html .= tag(
+                'img class="imagescroller_' . $btn . '" src="' . $img
+                . '" alt="' . $alt . '"' . ' style="' . $top . $left . '"'
             );
         }
-        $o .= '</div>';
-        return $o;
+        return $html;
     }
 
     /**
@@ -349,8 +231,6 @@ EOT;
      * @return string (X)HTML.
      *
      * @global array The paths of system files and folders.
-     *
-     * @todo Fix empty element.
      */
     protected function version()
     {
@@ -358,36 +238,7 @@ EOT;
 
         $iconPath = $pth['folder']['plugins'] . 'imagescroller/imagescroller.png';
         $version = IMAGESCROLLER_VERSION;
-        return <<<EOT
-<h1><a href="http://3-magi.net/?CMSimple_XH/Imagescroller_XH">
-    Imagescroller_XH</a></h1>
-<img src="$iconPath" with="128" height="128"
-     style="float: left; margin: 0 1em 0 0" alt="Plugin Icon" />
-<p>Version: $version</p>
-<p>Copyright &copy; 2012-2014 <a href="http://3-magi.net">
-    Christoph M. Becker</a></p>
-<p>Imagescroller_XH is powered by <a
-    href="http://flesler.blogspot.de/2008/02/jqueryserialscroll.html">
-    jQuery.SerialScroll</a>.</p>
-<p style="text-align: justify">
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-</p>
-<p style="text-align: justify">
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHAN&shy;TABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-</p>
-<p style="text-align: justify">
-    You should have received a copy of the GNU General Public License
-    along with this program. If not, see <a
-    href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.
-</p>
-
-EOT;
+        return $this->render('info', compact('iconPath', 'version'));
     }
 
     /**

TODO
@@ -4,5 +4,7 @@
 * translate to German
 * add meaningful alt texts
 * metaconfig.php
+* autoloading
+* update to PHPUnit 4.3
 
 * make responsive (sugg. by Tata)

classes/Gallery.php
@@ -1,0 +1,157 @@
+<?php
+
+/**
+ * The galleries.
+ *
+ * PHP version 5
+ *
+ * @category  CMSimple_XH
+ * @package   Imagescroller
+ * @author    Christoph M. Becker <cmbecker69@gmx.de>
+ * @copyright 2012-2014 Christoph M. Becker <http://3-magi.net/>
+ * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @version   SVN: $Id$
+ * @link      http://3-magi.net/?CMSimple_XH/Imagescroller_XH
+ */
+
+/**
+ * The galleries.
+ *
+ * @category CMSimple_XH
+ * @package  Imagescroller
+ * @author   Christoph M. Becker <cmbecker69@gmx.de>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @link     http://3-magi.net/?CMSimple_XH/Imagescroller_XH
+ */
+class Imagescroller_Gallery
+{
+    /**
+     * Returns a gallery made from an image folder.
+     *
+     * @param string $foldername A foldername.
+     *
+     * @return Imagescroller_Gallery
+     */
+    public static function makeFromFolder($foldername)
+    {
+        $gallery = new self();
+        $foldername = rtrim($foldername, '/') . '/';
+        if (($dir = opendir($foldername)) !== false) {
+            while (($filename = readdir($dir)) !== false) {
+                $fullFilename = $foldername . $filename;
+                if (self::isImageFile($fullFilename)) {
+                    $gallery->images[] = Imagescroller_Image::makeFromFilename(
+                        $fullFilename
+                    );
+                }
+            }
+            closedir($dir);
+        }
+        //natcasesort($imgs); // TODO: add back sorting
+        return $gallery;
+    }
+
+    /**
+     * Returns whether a file is an image file.
+     *
+     * @param string $filename A filename.
+     *
+     * @return bool
+     */
+    protected static function isImageFile($filename)
+    {
+        return is_file($filename) && getimagesize($filename);
+    }
+
+    /**
+     * Returns gallery made from an info file.
+     *
+     * @param string $filename A filename.
+     *
+     * @return Imagescroller_Gallery
+     */
+    public static function makeFromFile($filename)
+    {
+        $gallery = new self();
+        $foldername = dirname($filename) . '/';
+        $data = file_get_contents($filename);
+        $data = str_replace(array("\r\n", "\r"), "\n", $data);
+        $records = explode("\n\n", $data);
+        foreach ($records as $record) {
+            $record = array_map('trim', explode("\n", $record));
+            $record[0] = $foldername . $record[0];
+            $gallery->images[] = Imagescroller_Image::makeFromRecord($record);
+        }
+        return $gallery;
+    }
+
+    /**
+     * The images.
+     *
+     * @var array<Imagescroller_Image>
+     */
+    protected $images = array();
+
+    /**
+     * Returns the count of the images.
+     *
+     * @return int
+     */
+    public function getImageCount()
+    {
+        return count($this->images);
+    }
+
+    /**
+     * Returns the images.
+     *
+     * @return array<Imagescroller_Image>
+     */
+    public function getImages()
+    {
+        return $this->images;
+    }
+
+    /**
+     * Returns the dimensions of the gallery.
+     *
+     * If the dimensions differ, this will be reported through $e in admin mode.
+     *
+     * @return array
+     *
+     * @global string The (X)HTML containing error messages.
+     * @global bool   Whether we're in admin mode.
+     * @global array  The localization of the plugins.
+     *
+     * @todo Throw exceptions instead of appending to $e.
+     */
+    public function getDimensions()
+    {
+        global $e, $adm, $plugin_tx;
+
+        $ptx = $plugin_tx['imagescroller'];
+        foreach ($this->images as $image) {
+            $filename = $image->getFilename();
+            if (!is_readable($filename) || !($size = getimagesize($filename))) {
+                $e = '<li><strong>' . $ptx['error_no_image'] . '</strong>'
+                    . tag('br') . $filename . '</li>';
+                continue;
+            }
+            if (!isset($width)) {
+                list($width, $height) = $size;
+            } else {
+                if (($size[0] != $width || $size[1] != $height) && $adm) {
+                    $e .= '<li><strong>'
+                        . sprintf(
+                            $ptx['error_image_size'],
+                            $size[0], $size[1], $width, $height
+                        )
+                        . '</strong>' . tag('br') . $filename . '</li>';
+                }
+            }
+        }
+        return array($width, $height);
+    }
+}
+
+?>

classes/Image.php
@@ -1,0 +1,140 @@
+<?php
+
+/**
+ * The images.
+ *
+ * PHP version 5
+ *
+ * @category  CMSimple_XH
+ * @package   Imagescroller
+ * @author    Christoph M. Becker <cmbecker69@gmx.de>
+ * @copyright 2012-2014 Christoph M. Becker <http://3-magi.net/>
+ * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @version   SVN: $Id$
+ * @link      http://3-magi.net/?CMSimple_XH/Imagescroller_XH
+ */
+
+/**
+ * The images.
+ *
+ * @category CMSimple_XH
+ * @package  Imagescroller
+ * @author   Christoph M. Becker <cmbecker69@gmx.de>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @link     http://3-magi.net/?CMSimple_XH/Imagescroller_XH
+ */
+class Imagescroller_Image
+{
+    /**
+     * The filename.
+     *
+     * @var string
+     */
+    protected $filename;
+
+    /**
+     * The URL to link to.
+     *
+     * @var string
+     */
+    protected $url;
+
+    /**
+     * The title.
+     *
+     * @var string
+     */
+    protected $title;
+
+    /**
+     * The description.
+     *
+     * @var string
+     */
+    protected $description;
+
+
+    /**
+     * Creates an image from a filename.
+     *
+     * @param string $filename A filename.
+     *
+     * @return Imagescroller_Image
+     */
+    public static function makeFromFilename($filename)
+    {
+        $image = new self();
+        $image->filename = $filename;
+        return $image;
+    }
+
+    /**
+     * Creates an image from a data record.
+     *
+     * @param array $record A data record.
+     *
+     * @return Imagescroller_Image
+     */
+    public static function makeFromRecord($record)
+    {
+        $image = new self();
+        $image->filename = $record[0];
+        $image->url = isset($record[1]) ? $record[1] : '';
+        $image->title = isset($record[2]) ? $record[2] : '';
+        $image->description = isset($record[3]) ? $record[3] : '';
+        return $image;
+    }
+
+    /**
+     * Initializes a new instance.
+     */
+    public function __construct()
+    {
+        $this->filename = '';
+        $this->url = '';
+        $this->title = '';
+        $this->description = '';
+    }
+
+    /**
+     * Returns the filename.
+     *
+     * @return string
+     */
+    public function getFilename()
+    {
+        return $this->filename;
+    }
+
+    /**
+     * Returns the URL to link to.
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Returns the title.
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Returns the description.
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
+
+?>

css/stylesheet.css
@@ -1,18 +1,89 @@
-div.imagescroller_container {position: relative}
+.imagescroller_container {
+    position: relative;
+}
 
-div.imagescroller {position: relative; overflow: hidden}
-div.imagescroller ul {list-style: none; padding: 0; margin: 0}
-div.imagescroller li {position: relative; float:left; padding: 0; margin: 0}
-div.imagescroller a img {border: 0}
+.imagescroller {
+    position: relative;
+    overflow: hidden;
+}
+.imagescroller ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+}
+.imagescroller li {
+    position: relative;
+    float:left;
+    padding: 0;
+    margin: 0;
+}
+.imagescroller a img {
+    border: 0;
+}
 
-/* "media" buttons */
-img.imagescroller_prev {position: absolute; left: 10px; cursor: pointer; display: none}
-img.imagescroller_play {position: absolute; cursor: pointer; display: none; visibility: hidden}
-img.imagescroller_stop {position: absolute; cursor: pointer; display: none}
-img.imagescroller_next {position: absolute; right: 10px; cursor: pointer; display: none}
+/*
+ * "media" buttons
+ */
 
-/* info box */
-div.imagescroller_info {position: absolute; bottom: 50px; left: 70px; width: 30%; background: #333; opacity: 0.8; filter: alpha(opacity=80); padding: 0.5em}
-div.imagescroller_info h6 {margin: 0}
-div.imagescroller_info h6 a {color: #ff0; font-weight: bold}
-div.imagescroller_info p {color: #fff}
+.imagescroller_prev {
+    position: absolute;
+    left: 10px;
+    cursor: pointer;
+    display: none;
+}
+.imagescroller_play {
+    position: absolute;
+    cursor: pointer;
+    display: none;
+    visibility: hidden;
+}
+.imagescroller_stop {
+    position: absolute;
+    cursor: pointer;
+    display: none;
+}
+.imagescroller_next {
+    position: absolute;
+    right: 10px;
+    cursor: pointer;
+    display: none;
+}
+
+/*
+ * Info box
+ */
+
+.imagescroller_info {
+    position: absolute;
+    bottom: 50px;
+    left: 70px;
+    width: 30%;
+    background: #333;
+    opacity: 0.8;
+    filter: alpha(opacity=80);
+    padding: 0.5em;
+}
+.imagescroller_info h6 {
+    margin: 0;
+}
+.imagescroller_info h6 a {
+    color: #ff0;
+    font-weight: bold;
+}
+.imagescroller_info p {
+    color: #fff;
+}
+
+/*
+ * Back-end
+ */
+
+.imagescoller_logo {
+    float: left;
+    margin: 0 16px 0 0;
+    width: 128px;
+    height: 128px;
+}
+.imagescroller_license {
+    text-align: justify;
+}

imagescroller.js
@@ -1,0 +1,35 @@
+(function($) {
+    $(function() {
+        $(".imagescroller").serialScroll({
+            items: "li",
+            prev: ".imagescroller_container .imagescroller_prev",
+            next: ".imagescroller_container .imagescroller_next",
+            force: true,
+            axis: "xy",
+            duration: IMAGESCROLLER.duration,
+            interval: IMAGESCROLLER.interval,
+            constant: IMAGESCROLLER.constant
+        });
+        if (IMAGESCROLLER.dynamicControls) {
+            $(".imagescroller_container").mouseenter(function() {
+                $(this).find(".imagescroller_prev, .imagescroller_next," +
+                        ".imagescroller_play, .imagescroller_stop").show();
+            }).mouseleave(function() {
+                $(this).find(".imagescroller_prev, .imagescroller_next," +
+                        ".imagescroller_play, .imagescroller_stop").hide();
+            });
+            $(".imagescroller_stop").click(function() {
+                $(".imagescroller").trigger("stop");
+                $(".imagescroller_stop").css("visibility", "hidden");
+                $(".imagescroller_play").css("visibility", "visible");
+            });
+            $(".imagescroller_play").click(function() {
+                $(".imagescroller").trigger("start");
+                $(".imagescroller_play").css("visibility", "hidden");
+                $(".imagescroller_stop").css("visibility", "visible");
+            })
+        } else {
+            $(this).find(".imagescroller_prev, img.imagescroller_next").show();
+        }
+    })
+}(jQuery))

index.php
@@ -28,6 +28,16 @@ if (!defined('CMSIMPLE_XH_VERSION')) {
 require_once $pth['folder']['plugin_classes'] . 'Controller.php';
 
 /**
+ * The galleries.
+ */
+require_once $pth['folder']['plugin_classes'] . 'Gallery.php';
+
+/**
+ * The images.
+ */
+require_once $pth['folder']['plugin_classes'] . 'Image.php';
+
+/**
  * The version number.
  */
 define('IMAGESCROLLER_VERSION', '@IMAGESCROLLER_VERSION@');

tests/unit/ImageTest.php
@@ -1,0 +1,100 @@
+<?php
+
+/**
+ * Testing the images.
+ *
+ * PHP version 5
+ *
+ * @category  Testing
+ * @package   Imagescroller
+ * @author    Christoph M. Becker <cmbecker69@gmx.de>
+ * @copyright 2012-2014 Christoph M. Becker <http://3-magi.net/>
+ * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @version   SVN: $Id$
+ * @link      http://3-magi.net/?CMSimple_XH/Imagescroller_XH
+ */
+
+require_once './vendor/autoload.php';
+require_once './classes/Image.php';
+
+/**
+ * Testing the images.
+ *
+ * @category Testing
+ * @package  Imagescroller
+ * @author   Christoph M. Becker <cmbecker69@gmx.de>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @link     http://3-magi.net/?CMSimple_XH/Imagescroller_XH
+ */
+class ImageTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Tests that makeFromFilename() sets the filename.
+     *
+     * @return void
+     */
+    public function testMakeFromFilenameSetsFilename()
+    {
+        $filename = 'foo.bar';
+        $image = Imagescroller_Image::makeFromFilename($filename);
+        $this->assertEquals($filename, $image->getFilename());
+    }
+
+    /**
+     * Tests that makeFromRecord() sets the filename.
+     *
+     * @return void
+     */
+    public function testMakeFromRecordSetsFilename()
+    {
+        $filename = 'foo.bar';
+        $image = Imagescroller_Image::makeFromRecord(
+            array($filename, null, null, null)
+        );
+        $this->assertEquals($filename, $image->getFilename());
+    }
+
+    /**
+     * Tests that makeFromRecord() sets the URL to link to.
+     *
+     * @return void
+     */
+    public function testMakeFromRecordSetsUrl()
+    {
+        $url = 'http://example.com/';
+        $image = Imagescroller_Image::makeFromRecord(
+            array(null, $url, null, null)
+        );
+        $this->assertEquals($url, $image->getUrl());
+    }
+
+    /**
+     * Tests that makeFromRecord() sets the title.
+     *
+     * @return void
+     */
+    public function testMakeFromRecordSetsTitle()
+    {
+        $title = 'foo';
+        $image = Imagescroller_Image::makeFromRecord(
+            array(null, null, $title, null)
+        );
+        $this->assertEquals($title, $image->getTitle());
+    }
+
+    /**
+     * Tests that makeFromRecord() sets the description.
+     *
+     * @return void
+     */
+    public function testMakeFromRecordSetsDescription()
+    {
+        $description = 'foo';
+        $image = Imagescroller_Image::makeFromRecord(
+            array(null, null, null, $description)
+        );
+        $this->assertEquals($description, $image->getDescription());
+    }
+}
+
+?>

commit b873cc361c8c8329c156deaad50bba4604b6d2d7

Fix bug where gallery administration fataled
This regression had been introduced with commit
622b4161297f82a614818631b8d8137998d77f86.

classes/Controller.php
@@ -349,15 +349,15 @@ class Controller
         global $pth, $sn;
 
         $dn = "{$pth['folder']['images']}$_GET[imagescroller_gallery]";
-        $imgs = $this->imagesFromDir("$dn/");
+        $gallery = Gallery::makeFromFolder("$dn/");
         $url = "$sn?imagescroller&amp;admin=plugin_main";
         $o = "<form action=\"$url\" method=\"POST\"><table><tbody>";
-        foreach ($imgs as $img) {
+        foreach ($gallery->getImages() as $img) {
             $o .= '<tr><td>'
-                . tag("img src=\"$img\" width=\"200\" height=\"\" alt=\"\"")
+                . tag("img src=\"{$img->getFilename()}\" width=\"200\" height=\"\" alt=\"\"")
                 . tag(
                     'input type="hidden" name="imagescroller_image[]" value="'
-                    . $img . '"'
+                    . $img->getFilename() . '"'
                 )
                 . '</td>'
                 . '<td>'