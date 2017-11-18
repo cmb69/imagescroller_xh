cmb69/imagescroller_xh

commit 9201c4421ab530c8145eb8d7c4845a3d12eeb61b

Extract class MainController

classes/MainController.php
@@ -1,0 +1,98 @@
+<?php
+
+/*
+ * Copyright 2012-2017 Christoph M. Becker
+ *
+ * This file is part of Imagescroller_XH.
+ *
+ * Imagescroller_XH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Imagescroller_XH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Imagescroller_XH.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Imagescroller;
+
+use Pfw\View\View;
+use Pfw\View\HtmlString;
+
+class MainController
+{
+    /**
+     * @var string
+     */
+    private $path;
+
+    /**
+     * @param string $path
+     */
+    public function __construct($path)
+    {
+        $this->path = (string) $path;
+    }
+
+    /**
+     * @return void
+     */
+    public function defaultAction()
+    {
+        global $pth, $cf, $sl, $plugin_tx;
+
+        $contentfolder = $pth['folder']['content'];
+        if ($sl !== $cf['language']['default']) {
+            $contentfolder = dirname($contentfolder) . '/';
+        }
+        $contentfolder = "{$contentfolder}imagescroller/";
+        if (is_dir("{$pth['folder']['images']}{$this->path}")) {
+            $gallery = Gallery::makeFromFolder("{$pth['folder']['images']}{$this->path}");
+        } elseif (is_file("{$contentfolder}{$this->path}.txt")) {
+            $gallery = Gallery::makeFromFile("{$contentfolder}{$this->path}.txt");
+        } else {
+            return XH_message('fail', $plugin_tx['imagescroller']['error_gallery_missing'], $this->path);
+        }
+        list($width, $height) = $gallery->getDimensions();
+        Controller::emitJs();
+        $totalWidth = $gallery->getImageCount() * $width;
+        $renderedButtons = new HtmlString($this->renderButtons($width, $height));
+        (new View('imagescroller'))
+            ->template('gallery')
+            ->data(compact('gallery', 'width', 'height', 'totalWidth', 'renderedButtons'))
+            ->render();
+    }
+
+    /**
+     * @param int $width
+     * @param int $height
+     * @return string
+     */
+    private function renderButtons($width, $height)
+    {
+        global $pth, $plugin_tx;
+
+        $html = '';
+        foreach (array('prev', 'next', 'play', 'stop') as $btn) {
+            $name = $btn;
+            $alt = $plugin_tx['imagescroller']['button_' . $btn];
+            $img = $pth['folder']['plugins'] . 'imagescroller/images/' . $name
+                . '.png';
+            list($w, $h) = getimagesize($img);
+            $top = 'top:' . intval(($height - $h) / 2) . 'px;';
+            $left = ($btn == 'play' || $btn == 'stop')
+                ? 'left:' . intval(($width - $w) / 2) . 'px'
+                : '';
+            $html .= tag(
+                'img class="imagescroller_' . $btn . '" src="' . $img
+                . '" alt="' . $alt . '"' . ' style="' . $top . $left . '"'
+            );
+        }
+        return $html;
+    }
+}

classes/Controller.php
@@ -22,8 +22,6 @@
 namespace Imagescroller;
 
 use Pfw\SystemCheckService;
-use Pfw\View\View;
-use Pfw\View\HtmlString;
 
 class Controller
 {
@@ -35,7 +33,7 @@ class Controller
         global $plugin_cf;
 
         if ($plugin_cf['imagescroller']['autoload']) {
-            $this->emitJs();
+            self::emitJs();
         }
         if (XH_ADM) {
             if (XH_wantsPluginAdministration('imagescroller')) {
@@ -78,7 +76,7 @@ class Controller
     /**
      * @return void
      */
-    public function emitJs()
+    public static function emitJs()
     {
         global $pth, $hjs, $plugin_cf;
         static $again = false;
@@ -104,66 +102,6 @@ class Controller
             . '/* ]]> */</script>'
             . '<script type="text/javascript" src="' . $pth['folder']['plugins']
             . 'imagescroller/imagescroller.js"></script>';
-    }
-
-    /**
-     * @param string $path
-     * @return string
-     */
-    public function main($path)
-    {
-        global $pth, $cf, $sl, $plugin_tx;
-
-        $contentfolder = $pth['folder']['content'];
-        if ($sl !== $cf['language']['default']) {
-            $contentfolder = dirname($contentfolder) . '/';
-        }
-        $contentfolder = "{$contentfolder}imagescroller/";
-        if (is_dir("{$pth['folder']['images']}$path")) {
-            $gallery = Gallery::makeFromFolder("{$pth['folder']['images']}$path");
-        } elseif (is_file("{$contentfolder}$path.txt")) {
-            $gallery = Gallery::makeFromFile("{$contentfolder}$path.txt");
-        } else {
-            return XH_message('fail', $plugin_tx['imagescroller']['error_gallery_missing'], $path);
-        }
-        list($width, $height) = $gallery->getDimensions();
-        $this->emitJs();
-        $totalWidth = $gallery->getImageCount() * $width;
-        $renderedButtons = new HtmlString($this->renderButtons($width, $height));
-        ob_start();
-        (new View('imagescroller'))
-            ->template('gallery')
-            ->data(compact('gallery', 'width', 'height', 'totalWidth', 'renderedButtons'))
-            ->render();
-        return ob_get_clean();
-    }
-
-    /**
-     * @param int $width
-     * @param int $height
-     * @return string
-     */
-    protected function renderButtons($width, $height)
-    {
-        global $pth, $plugin_tx;
-
-        $html = '';
-        foreach (array('prev', 'next', 'play', 'stop') as $btn) {
-            $name = $btn;
-            $alt = $plugin_tx['imagescroller']['button_' . $btn];
-            $img = $pth['folder']['plugins'] . 'imagescroller/images/' . $name
-                . '.png';
-            list($w, $h) = getimagesize($img);
-            $top = 'top:' . intval(($height - $h) / 2) . 'px;';
-            $left = ($btn == 'play' || $btn == 'stop')
-                ? 'left:' . intval(($width - $w) / 2) . 'px'
-                : '';
-            $html .= tag(
-                'img class="imagescroller_' . $btn . '" src="' . $img
-                . '" alt="' . $alt . '"' . ' style="' . $top . $left . '"'
-            );
-        }
-        return $html;
     }
 
     /**

index.php
@@ -48,9 +48,9 @@ $_Imagescroller_controller = new Imagescroller\Controller();
  */
 function imagescroller($path)
 {
-    global $_Imagescroller_controller;
-
-    return $_Imagescroller_controller->main($path);
+    ob_start();
+    (new Imagescroller\MainController($path))->defaultAction();
+    return ob_get_clean();
 }
 
 $_Imagescroller_controller->dispatch();

commit 81eebccd879f876daefac6c8b698e59cfe2bc459

Actually show error message regarding missing gallery
The regression has been introduced in
9201c4421ab530c8145eb8d7c4845a3d12eeb61b.

classes/MainController.php
@@ -56,7 +56,8 @@ class MainController
         } elseif (is_file("{$contentfolder}{$this->path}.txt")) {
             $gallery = Gallery::makeFromFile("{$contentfolder}{$this->path}.txt");
         } else {
-            return XH_message('fail', $plugin_tx['imagescroller']['error_gallery_missing'], $this->path);
+            echo XH_message('fail', $plugin_tx['imagescroller']['error_gallery_missing'], $this->path);
+            return;
         }
         list($width, $height) = $gallery->getDimensions();
         Plugin::emitJs();