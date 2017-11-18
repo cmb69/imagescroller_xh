39f34d903f46bb20e22744c400c87127e8ff4b85
Christoph M. Becker <cmbecker69@gmx.de>
2017-11-18 18:06:20 +0100

    Remove superfluous closure

--- imagescroller.js
+++ imagescroller.js
@@ -1,35 +1,33 @@
-(function($) {
-    $(function() {
-        $(".imagescroller").serialScroll({
-            items: ".imagescroller_item",
-            prev: ".imagescroller_container .imagescroller_prev",
-            next: ".imagescroller_container .imagescroller_next",
-            force: true,
-            axis: "xy",
-            duration: IMAGESCROLLER.duration,
-            interval: IMAGESCROLLER.interval,
-            constant: IMAGESCROLLER.constant
+jQuery(function($) {
+    $(".imagescroller").serialScroll({
+        items: ".imagescroller_item",
+        prev: ".imagescroller_container .imagescroller_prev",
+        next: ".imagescroller_container .imagescroller_next",
+        force: true,
+        axis: "xy",
+        duration: IMAGESCROLLER.duration,
+        interval: IMAGESCROLLER.interval,
+        constant: IMAGESCROLLER.constant
+    });
+    if (IMAGESCROLLER.dynamicControls) {
+        $(".imagescroller_container").mouseenter(function() {
+            $(this).find(".imagescroller_prev, .imagescroller_next," +
+                    ".imagescroller_play, .imagescroller_stop").css("visibility", "visible");
+        }).mouseleave(function() {
+            $(this).find(".imagescroller_prev, .imagescroller_next," +
+                    ".imagescroller_play, .imagescroller_stop").css("visibility", "hidden");
         });
-        if (IMAGESCROLLER.dynamicControls) {
-            $(".imagescroller_container").mouseenter(function() {
-                $(this).find(".imagescroller_prev, .imagescroller_next," +
-                        ".imagescroller_play, .imagescroller_stop").css("visibility", "visible");
-            }).mouseleave(function() {
-                $(this).find(".imagescroller_prev, .imagescroller_next," +
-                        ".imagescroller_play, .imagescroller_stop").css("visibility", "hidden");
-            });
-            $(".imagescroller_stop").click(function() {
-                $(".imagescroller").trigger("stop");
-                $(".imagescroller_stop").css("display", "none");
-                $(".imagescroller_play").css("display", "inline");
-            });
-            $(".imagescroller_play").click(function() {
-                $(".imagescroller").trigger("start");
-                $(".imagescroller_play").css("display", "none");
-                $(".imagescroller_stop").css("display", "inline");
-            })
-        } else {
-            $(this).find(".imagescroller_prev, .imagescroller_next").css("visibility", "visible");
-        }
-    })
-}(jQuery))
+        $(".imagescroller_stop").click(function() {
+            $(".imagescroller").trigger("stop");
+            $(".imagescroller_stop").css("display", "none");
+            $(".imagescroller_play").css("display", "inline");
+        });
+        $(".imagescroller_play").click(function() {
+            $(".imagescroller").trigger("start");
+            $(".imagescroller_play").css("display", "none");
+            $(".imagescroller_stop").css("display", "inline");
+        })
+    } else {
+        $(this).find(".imagescroller_prev, .imagescroller_next").css("visibility", "visible");
+    }
+});
