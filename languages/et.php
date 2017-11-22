5990ead8b48b315a5aac044d8556a1ad6bd07acf
Christoph M. Becker <cmbecker69@gmx.de>
2017-11-22 18:52:45 +0100

    Fix CS violations in config and language files

--- languages/et.php
+++ languages/et.php
@@ -1,11 +1,11 @@
 <?php
 
-	$plugin_tx['imagescroller']['button_prev']="Eelmine foto";
-	$plugin_tx['imagescroller']['button_next']="Järgmine foto";
-	$plugin_tx['imagescroller']['button_play']="Jätka slaidiseanssi";
-	$plugin_tx['imagescroller']['button_stop']="Peata slaidiseanss";
-	$plugin_tx['imagescroller']['error_no_image']="Fotod puuduvad:";
-	$plugin_tx['imagescroller']['error_image_size']="Foto suurus %1dx%2d (eeldatav %3dx%4d):";
-	$plugin_tx['imagescroller']['cf_scroll_interval']="Kahe kerimise vaheline intervall millisekundites.";
-	$plugin_tx['imagescroller']['cf_scroll_duration']="Järgmise pildi kerimise aeg millisekundites.";
-	$plugin_tx['imagescroller']['cf_autoload']="Kui JS automaatne laadimine on vajalik: 1 (jah) või 0 (ei). Valige 1, kui kasutatate malliga.";
+$plugin_tx['imagescroller']['button_prev']="Eelmine foto";
+$plugin_tx['imagescroller']['button_next']="Järgmine foto";
+$plugin_tx['imagescroller']['button_play']="Jätka slaidiseanssi";
+$plugin_tx['imagescroller']['button_stop']="Peata slaidiseanss";
+$plugin_tx['imagescroller']['error_no_image']="Fotod puuduvad:";
+$plugin_tx['imagescroller']['error_image_size']="Foto suurus %1dx%2d (eeldatav %3dx%4d):";
+$plugin_tx['imagescroller']['cf_scroll_interval']="Kahe kerimise vaheline intervall millisekundites.";
+$plugin_tx['imagescroller']['cf_scroll_duration']="Järgmise pildi kerimise aeg millisekundites.";
+$plugin_tx['imagescroller']['cf_autoload']="Kui JS automaatne laadimine on vajalik: 1 (jah) või 0 (ei). Valige 1, kui kasutatate malliga.";
